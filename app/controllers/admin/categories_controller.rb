70fffdd9533af9e35843e9f0f7464b28630671cc
Tom Zhang <tom.zhang989@gmail.com>
2021-11-11 14:13:31 -0500

    implements category creation

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -10,12 +10,26 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new(product_params)
+    @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :category], notice: 'Category created!'
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to [:admin, :categories], notice: 'Category deleted!'
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
-  resources :categories, only: [:index, :show]
+  resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -19,7 +19,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, :categories, except: [:edit, :update, :show]
+    resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:delete, :edit, :update, :show]
   end
 
   
