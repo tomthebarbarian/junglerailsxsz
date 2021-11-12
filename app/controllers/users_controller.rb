242077fed16bc564e33debc227e13d8a09a2915b
Tom Zhang <tom.zhang989@gmail.com>
2021-11-11 19:09:14 -0500

    adds ownership of orders to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,11 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
+    params.require(:user).permit(
+      :name,
+      :password,
+      :password_confirmation
+    )
   end
 
 end
