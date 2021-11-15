6ab223942ca05d6e957f0e1666e1508c042aa9ff
Tom Zhang <tom.zhang989@gmail.com>
2021-11-15 16:47:46 -0500

    adds model for product testing

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it "has a name" do
+
+    end
+    it "has a price" do
+
+    end
+    it "has a quantity" do
+
+    end
+    it "has a presence" do
+      
+    end
+  end
+end
