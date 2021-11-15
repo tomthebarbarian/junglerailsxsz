c53f6d85ad56be0037751b95be4efff407bfdcde
Tom Zhang <tom.zhang989@gmail.com>
2021-11-15 18:50:19 -0500

    adds the firsname/lastname requirements and controllers to the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   has_many :orders
   has_secure_password
 
-  validates :username, presence: true
+  validates :email, presence: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,9 @@ ActiveRecord::Schema.define(version: 20211111234549) do
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
+    t.string   "email"
+    t.string   "firstname"
+    t.string   "lastname"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
